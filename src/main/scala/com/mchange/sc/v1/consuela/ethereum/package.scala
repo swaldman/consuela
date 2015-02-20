2ed645983472c867aa5a7f9fe52031936f516563
Steve Waldman <swaldman@mchange.com>
2015-02-20 00:15:17 -0800

    More organization of Nibbles, alias of EthHash all in package objects.

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/package.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/package.scala
@@ -3,12 +3,19 @@ package com.mchange.sc.v1.consuela;
 package object ethereum {
   type Nibble = Int;
 
-  def nibbles( bytes : Seq[Byte] ) = lsbNibbles( bytes.map( _ & 0xFF ) )
+  val Nibbles = (0x0 to 0xF).toIndexedSeq
 
   /**
-   *  Least significant byte nibbles
+   *  @return an always-even sequence of Ints between 0x0 and 0xF
+   */  
+  def toNibbles( bytes : Seq[Byte] ) : Seq[Nibble] = lsbNibbles( bytes.map( _ & 0xFF ) )
+
+  /**
+   *  Least significant byte nibbles, ignores top three bytes!
+   * 
+   *  @return an always-even sequence of Ints between 0x0 and 0xF
    */ 
-  def lsbNibbles( bytes : Seq[Int] ) : Seq[Int] = {
+  def lsbNibbles( bytes : Seq[Int] ) : Seq[Nibble] = {
     val out = new Array[Int]( bytes.length * 2 );
     var idx = 0;
     bytes.foreach {  byte => 
--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/trie/package.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/trie/package.scala
@@ -1,5 +1,8 @@
 package com.mchange.sc.v1.consuela.ethereum;
 
+import com.mchange.sc.v1.consuela.hash.Hash;
+
 package object trie {
+  type EthHash = Hash.SHA3_256;
 }
 
