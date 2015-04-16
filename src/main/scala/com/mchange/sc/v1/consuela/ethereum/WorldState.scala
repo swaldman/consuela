09c0ed417b534cb8ec6f6d5b4df3a4d9df7a1791
Steve Waldman <swaldman@mchange.com>
2015-04-16 00:57:12 -0700

    Reorg of WorldState to render contract and agent accounts type-system distinct.

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/WorldState.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/WorldState.scala
@@ -12,12 +12,18 @@ object WorldState {
   object Account {
     def encodeRLP( account : Account ) : Seq[Byte] = {
       import RLP._;
+      val codeHash = {
+        account match {
+          case contract : Contract => contract.codeHash;
+          case agent    : Agent    => EmptyTrieHash;
+        }
+      }
       val encodable = Encodable.Seq( 
         Seq(
           Encodable.UnsignedBigInt( account.nonce ),
           Encodable.UnsignedBigInt( account.balance ),
           Encodable.ByteSeq( account.storageRoot.bytes ),
-          Encodable.ByteSeq( account.codeHash.bytes )
+          Encodable.ByteSeq( codeHash.bytes )
         )
       );
       encode( encodable )
@@ -27,7 +33,12 @@ object WorldState {
       import Encodable.{ByteSeq => BS}
       val (encodable, rest) = decode( bytes );
       val Encodable.Seq( Seq( BS( nonceBytes ), BS( balanceBytes ), BS( storageRootBytes ), BS( codeHashBytes ) ) ) = encodable;
-      val account = Account( BigInt(1, nonceBytes.toArray), BigInt(1, balanceBytes.toArray), EthHash.withBytes( storageRootBytes ), EthHash.withBytes( codeHashBytes ) );
+      val account = {
+        EthHash.withBytes( codeHashBytes ) match {
+          case EmptyTrieHash => Agent( BigInt(1, nonceBytes.toArray), BigInt(1, balanceBytes.toArray), EthHash.withBytes( storageRootBytes ) );
+          case codeHash      => Contract( BigInt(1, nonceBytes.toArray), BigInt(1, balanceBytes.toArray), EthHash.withBytes( storageRootBytes ), codeHash );
+        }
+      }
       ( account, rest )
     }
     def decodeCompleteRLP( bytes : Seq[Byte] ) : Account = {
@@ -40,18 +51,32 @@ object WorldState {
         account
       }
     }
+    case class Contract( nonce : BigInt, balance : BigInt, storageRoot : EthHash, codeHash : EthHash ) extends Account {
+      require( (nonce elem_: Unsigned256) && (balance elem_: Unsigned256) && codeHash != EmptyTrieHash );
+    }
+    case class Agent( nonce : BigInt, balance : BigInt, storageRoot : EthHash ) extends Account {
+      require( (nonce elem_: Unsigned256) && (balance elem_: Unsigned256) );
+
+      def codeHash = EmptyTrieHash;
+    }
   }
-  case class Account( nonce : BigInt, balance : BigInt, storageRoot : EthHash, codeHash : EthHash ) {
-    require( (nonce elem_: Unsigned256) && (balance elem_: Unsigned256) );
+  sealed trait Account {
+    def nonce       : BigInt;
+    def balance     : BigInt;
+    def storageRoot : EthHash;
+    def codeHash    : EthHash;
 
     lazy val rlpBytes = Account.encodeRLP( this );
+
+    def isAgent    : Boolean = codeHash == EmptyTrieHash;
+    def isContract : Boolean = !this.isAgent;
   }
 }
 class WorldState( private val trie : SimpleEthTrie ) {
   import WorldState.Account;
 
   def this( db : EthTrieDb, rootHash : EthHash ) = this( new SimpleEthTrie( db, rootHash ) );
-  def this( db : EthTrieDb ) = this( new SimpleEthTrie( db, EthTrieDb.EmptyHash ) )
+  def this( db : EthTrieDb ) = this( new SimpleEthTrie( db, EmptyTrieHash ) )
 
   val RootHash = trie.RootHash;
 
