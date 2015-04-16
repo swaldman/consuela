70a4981f5bb8edb58773f8b15e90a568ee832c73
Steve Waldman <swaldman@mchange.com>
2015-04-16 06:54:53 -0700

    Cleaned up EthTransaction API to reflect fact that to is not optional on Message transactiions.

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/EthTransaction.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/EthTransaction.scala
@@ -13,12 +13,12 @@ import specification.Set.Unsigned256;
 
 object EthTransaction {
   object Abstract {
-    abstract class Message( nonce : BigInt, gasPrice : BigInt, gasLimit : BigInt, to : EthAddress, value : BigInt, dataBytes : Array[Byte] ) 
-        extends Abstract( nonce, gasPrice, gasLimit, Some(to), value, dataBytes ) {
+    abstract class Message( nonce : BigInt, gasPrice : BigInt, gasLimit : BigInt, val to : EthAddress, value : BigInt, dataBytes : Array[Byte] ) 
+        extends Abstract( nonce, gasPrice, gasLimit, Some(to), value, dataBytes ) with EthTransaction.Message {
       def data : IndexedSeq[Byte] = payload;
     }
     abstract class ContractCreation( nonce : BigInt, gasPrice : BigInt, gasLimit : BigInt, value : BigInt, initBytes : Array[Byte] ) 
-        extends Abstract( nonce, gasPrice, gasLimit, None, value, initBytes ) {
+        extends Abstract( nonce, gasPrice, gasLimit, None, value, initBytes )  with EthTransaction.ContractCreation {
       def init : IndexedSeq[Byte] = payload;
     }
   }
@@ -26,7 +26,7 @@ object EthTransaction {
     val nonce : BigInt,
     val gasPrice : BigInt, 
     val gasLimit : BigInt, 
-    val to : Option[EthAddress], 
+    val mbto : Option[EthAddress], 
     val value : BigInt, 
     private[EthTransaction] val payloadBytes : Array[Byte] 
   ) extends EthTransaction {
@@ -38,18 +38,18 @@ object EthTransaction {
       this.nonce == other.nonce &&
       this.gasPrice == other.gasPrice &&
       this.gasLimit == other.gasLimit &&
-      this.to == other.to &&
+      this.mbto == other.mbto &&
       Arrays.equals( this.payloadBytes, other.payloadBytes )
     }
-    protected lazy val baseHash = nonce.## ^ gasPrice.## ^ gasLimit.## ^ to.## ^ MurmurHash3.bytesHash( payloadBytes )
+    protected lazy val baseHash = nonce.## ^ gasPrice.## ^ gasLimit.## ^ mbto.## ^ MurmurHash3.bytesHash( payloadBytes )
 
     protected def encodableNonce    = RLP.Encodable.UnsignedBigInt( nonce );
     protected def encodableGasPrice = RLP.Encodable.UnsignedBigInt( gasPrice );
-    protected def encodableTo       = to.fold( RLP.Encodable.EmptyByteSeq )( address => RLP.Encodable.ByteSeq( address.bytes ) );
+    protected def encodableMbto       = mbto.fold( RLP.Encodable.EmptyByteSeq )( address => RLP.Encodable.ByteSeq( address.bytes ) );
     protected def encodableValue    = RLP.Encodable.UnsignedBigInt( value );
     protected def encodablePayload  = RLP.Encodable.ByteSeq( payloadBytes );
 
-    protected def baseRlpElements : Seq[RLP.Encodable] = Seq( encodableNonce, encodableGasPrice, encodableTo, encodableValue, encodablePayload );
+    protected def baseRlpElements : Seq[RLP.Encodable] = Seq( encodableNonce, encodableGasPrice, encodableMbto, encodableValue, encodablePayload );
     protected def baseRlp         : Seq[Byte]          = RLP.encode( RLP.Encodable.Seq( baseRlpElements ) );
   }
   object Unsigned {
@@ -90,7 +90,7 @@ object EthTransaction {
         new Message( nonce, gasPrice, gasLimit, to, value, dataBytes.clone(), signature )
       }
       private[EthTransaction] def apply( unsigned : Unsigned.Message, signature : EthSignature ) = { // no need for defensive array clone(), both signed and unsigned are immutable
-        new Message( unsigned.nonce, unsigned.gasPrice, unsigned.gasLimit, unsigned.to.get, unsigned.value, unsigned.payloadBytes, signature )
+        new Message( unsigned.nonce, unsigned.gasPrice, unsigned.gasLimit, unsigned.to, unsigned.value, unsigned.payloadBytes, signature )
       }
     }
     final class Message private ( nonce : BigInt, gasPrice : BigInt, gasLimit : BigInt, to : EthAddress, value : BigInt, dataBytes : Array[Byte], val signature : EthSignature ) 
@@ -139,20 +139,28 @@ object EthTransaction {
     lazy val rlpBytes : Seq[Byte] = RLP.encode( RLP.Encodable.Seq( baseRlpElements ++ sigRlpElements ) );
   }
   trait Message extends EthTransaction {
+    def to   : EthAddress; // not optional once we know we are a message
     def data : IndexedSeq[Byte];
+
+    def isMessage = true;
   }
   trait ContractCreation extends EthTransaction {
     def init : IndexedSeq[Byte];
+
+    def isMessage = true;
   }
 }
 sealed trait EthTransaction {
   def nonce    : BigInt;
   def gasPrice : BigInt;
   def gasLimit : BigInt;
-  def to       : Option[EthAddress];
+  def mbto     : Option[EthAddress];
   def value    : BigInt;
 
   def signed   : Boolean;
+
+  def isMessage          : Boolean;
+  def isContractCreation : Boolean = !this.isMessage;
 }
 
 
