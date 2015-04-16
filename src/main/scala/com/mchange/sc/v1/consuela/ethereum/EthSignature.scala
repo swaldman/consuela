a60a2c1dfe0c96fc864f1412af8b10cec34132b4
Steve Waldman <swaldman@mchange.com>
2015-04-16 05:08:13 -0700

    Some convenience methods on EthSignature

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/EthSignature.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/EthSignature.scala
@@ -1,5 +1,6 @@
 package com.mchange.sc.v1.consuela.ethereum;
 
+import com.mchange.sc.v1.consuela._;
 import com.mchange.sc.v1.consuela.Implicits._;
 import scala.collection._;
 
@@ -9,6 +10,13 @@ import specification.Set.SignatureS;
 
 case class EthSignature( val v : Byte, val r : BigInt, val s : BigInt ) {
   require( (v elem_!: SignatureV) && (r elem_!: SignatureR) && (s elem_!: SignatureS) );
+
+  def bytesWereSigned( bytes : Array[Byte] ) : Option[EthPublicKey] = {
+    crypto.secp256k1.BouncyCastlePublicKeyComputer.recoverPublicKeyBytesV( v, r.bigInteger, s.bigInteger, bytes ).map( EthPublicKey(_) )
+  }
+
+  def hashWasSigned( hash : EthHash )        : Option[EthPublicKey] = bytesWereSigned( hash.toByteArray );
+  def hashWasSigned( bytes : Array[Byte] )   : Option[EthPublicKey] = hashWasSigned( EthHash.hash( bytes ) );
 
   lazy val exportByteSeq = v.toByte +: Vector( (r.unsignedBytes(32) ++ s.unsignedBytes(32)) : _* ); 
 }
