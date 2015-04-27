a920a2542e6a2ccc9ac7d272d79ee0f7988ba127
Steve Waldman <swaldman@mchange.com>
2015-04-26 21:18:04 -0700

    Added poop partial function in association with Failable[T], to enable 'try somethingDangerous() catch poop;' idiom.

--- src/main/scala/com/mchange/sc/v1/consuela/package.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/package.scala
@@ -87,6 +87,8 @@ package object consuela {
 
   def succeed[T]( value : T) : Failable[T] = Right( value );
 
+  val poop : PartialFunction[Throwable, Failable[Nothing]] = { case scala.util.control.NonFatal( t : Throwable ) => fail( t ) }
+
   implicit class FailableTry[T]( val attempt : Try[T] ) extends AnyVal {
     def toFailable : Failable[T] = attempt match {
       case Success( value )     => succeed( value );
