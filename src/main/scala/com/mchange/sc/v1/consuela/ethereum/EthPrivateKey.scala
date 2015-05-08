a8be72d190713691cec979f6bf465250bf35749f
Steve Waldman <swaldman@mchange.com>
2015-05-08 06:54:44 -0700

    Reimplemented EthPrivateKey as ByteSeqValue.

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/EthPrivateKey.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/EthPrivateKey.scala
@@ -5,25 +5,32 @@ import com.mchange.sc.v1.consuela.crypto;
 
 import specification.Types.{SignatureV, SignatureR, SignatureS}
 
-import com.mchange.sc.v1.consuela.util.ByteArrayValue;
+import com.mchange.sc.v1.consuela.util.ByteSeqValue;
+
+import com.mchange.sc.v2.collection.immutable.ImmutableArraySeq;
 
 import java.security.SecureRandom;
 
 object EthPrivateKey {
   val ByteLength = crypto.secp256k1.ValueByteLength;
 
-  def apply( bytes  : Seq[Byte] )    : EthPrivateKey = new EthPrivateKey( bytes.toArray );
-  def apply( bytes  : Array[Byte] )  : EthPrivateKey = new EthPrivateKey( bytes.clone() );
-  def apply( bigInt : BigInt )       : EthPrivateKey = new EthPrivateKey( bigInt.unsignedBytes( ByteLength ) );
+  def apply( bytes  : Seq[Byte] )    : EthPrivateKey = {
+    bytes match {
+      case iasb : ImmutableArraySeq.Byte => new EthPrivateKey( iasb );
+      case other                         => new EthPrivateKey( ImmutableArraySeq.Byte( other.toArray ) );
+    }
+  }
+  def apply( bytes  : Array[Byte] )  : EthPrivateKey = new EthPrivateKey( ImmutableArraySeq.Byte( bytes ) );
+  def apply( bigInt : BigInt )       : EthPrivateKey = new EthPrivateKey( ImmutableArraySeq.Byte.createNoCopy( bigInt.unsignedBytes( ByteLength ) ) );
   def apply( random : SecureRandom ) : EthPrivateKey = {
     val bytes = Array.ofDim[Byte](ByteLength);
     random.nextBytes( bytes );
-    new EthPrivateKey( bytes );
+    new EthPrivateKey( ImmutableArraySeq.Byte.createNoCopy( bytes ) )
   }
 }
 
-final class EthPrivateKey private( protected val _bytes : Array[Byte] ) extends ByteArrayValue with ByteArrayValue.UnsignedBigIntegral {
-  require( _bytes.length == EthPrivateKey.ByteLength );
+final class EthPrivateKey private( val bytes : ImmutableArraySeq.Byte ) extends ByteSeqValue with ByteSeqValue.UnsignedBigIntegral {
+  require( bytes.length == EthPrivateKey.ByteLength );
 
   def s = this.toBigInt;
 
