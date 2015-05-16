71433647cf2dddbcde3489e88c21251056e100e5
Steve Waldman <swaldman@mchange.com>
2015-05-16 03:50:14 -0700

    Minor simplification of doHash(...) methods.

--- src/main/scala/com/mchange/sc/v1/consuela/hash/package.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/hash/package.scala
@@ -1,20 +1,19 @@
 package com.mchange.sc.v1.consuela;
 
 import java.io.InputStream;
-import java.io.ByteArrayInputStream;
 
 import javax.crypto._;
 import java.security._;
 import java.security.interfaces._;
 import java.security.spec._;
 
-import com.mchange.sc.v2.lang.borrow;
-
 package object hash {
 
-  def doHash( algoName : String, is : InputStream )( implicit provider : crypto.jce.Provider ) : Array[Byte] = {
-    val md = MessageDigest.getInstance(algoName, provider.name );
-    val buffer = new Array[Byte](1024);
+  val DefaultBufferSize = 1024 * 1024;
+
+  def doHash( algoName : String, is : InputStream, bufferSize : Int = DefaultBufferSize )( implicit provider : crypto.jce.Provider ) : Array[Byte] = {
+    val md = MessageDigest.getInstance( algoName, provider.name );
+    val buffer = new Array[Byte]( bufferSize );
     var count = is.read( buffer );
     while ( count >= 0 ) {
       md.update( buffer, 0, count );
@@ -23,7 +22,9 @@ package object hash {
     md.digest()
   }
   def doHash( algoName : String, bytes : Array[Byte] )( implicit provider : crypto.jce.Provider ) : Array[Byte] = {
-    borrow ( new ByteArrayInputStream( bytes ) ) { bais => doHash( algoName, bais )( provider ) }
+    val md = MessageDigest.getInstance(algoName, provider.name );
+    md.update( bytes, 0, bytes.length );
+    md.digest()
   }
   def doHash( algoName : String, bytes : Seq[Byte] )( implicit provider : crypto.jce.Provider ) : Array[Byte] = doHash( algoName, bytes.toArray )( provider )
 }
