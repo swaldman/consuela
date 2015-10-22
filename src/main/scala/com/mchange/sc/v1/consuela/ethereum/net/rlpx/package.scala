7af2cac0ac4531666c46556ec19291d1590fa2bf
Steve Waldman <swaldman@mchange.com>
2015-10-22 00:23:29 -0700

    Implement RLPX ReceiverHandshakeMessage.

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/net/rlpx/package.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/net/rlpx/package.scala
@@ -13,17 +13,31 @@ package object rlpx {
     signature          : EthSignature,
     ephemeralKeyMac    : EthHash,
     permanentPublicKey : EthPublicKey,
-    nonce              : ByteSeqExact32,
-    knownPeer          : Unsigned1
+    initiatorNonce     : ByteSeqExact32,
+    initiatorKnownPeer : Unsigned1
   ) {
     lazy val bytes : ByteSeqExact194 = {
-      val buff = new mutable.ArrayBuffer[Byte]( 194 )
+      val buff = new mutable.ArrayBuffer[Byte]( 194 ) // serializes to exactly 194 bytes
       buff ++= signature.exportBytesRSV.widen
       buff ++= ephemeralKeyMac.bytes
       buff ++= permanentPublicKey.toByteSeqExact65.widen
-      buff ++= nonce.widen
-      buff += knownPeer.widen // just one Byte
+      buff ++= initiatorNonce.widen
+      buff += initiatorKnownPeer.widen // just one Byte
       ByteSeqExact194.assert( ImmutableArraySeq.Byte( buff.toArray ) )
+    }
+  }
+
+  case class ReceiverHandshakeMessage (
+    receiverEcdheRandomPubkey : EthPublicKey,
+    receiverNonce             : ByteSeqExact32,
+    receiverKnownPeer         : Unsigned1
+  ) {
+    lazy val bytes : ByteSeqExact97 = {
+      val buff = new mutable.ArrayBuffer[Byte]( 97 ) // serializes to exactly 97 bytes
+      buff ++= receiverEcdheRandomPubkey.toByteSeqExact64.widen
+      buff ++= receiverNonce.widen
+      buff +=  receiverKnownPeer.widen // just one Byte
+      ByteSeqExact97.assert( ImmutableArraySeq.Byte( buff.toArray ) )
     }
   }
 
