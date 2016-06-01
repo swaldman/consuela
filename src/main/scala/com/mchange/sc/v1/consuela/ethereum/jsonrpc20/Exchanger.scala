444ec00044adae1a6509e8164f1c9b8e047d5eb4
Steve Waldman <swaldman@mchange.com>
2016-06-01 13:28:54 -0700

    More work on play-ws Exhanger for jsonrpc20

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/jsonrpc20/Exchanger.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/jsonrpc20/Exchanger.scala
@@ -10,6 +10,10 @@ object Exchanger {
 
   /**
     * This seems way overcomplicated.
+    * 
+    * It also brings in pretty much the whole Play/Akka ecosystem via transitive dependencies.
+    * 
+    * Maybe segregregate and choose a simpler, cleaner HTTP client by default.
     */ 
   final object Play {
     import play.api.libs.ws._
@@ -35,7 +39,7 @@ object Exchanger {
         // MT: protected by this' lock
         private var children = mutable.HashSet.empty[WSClient]
 
-        def register( wsClient : WSClient ) : Unit = this.synchronized( if ( children != null ) children += wsClient else throw new Manager.ClosedException(this) )
+        def register( wsClient : WSClient )   : Unit = this.synchronized( if ( children != null ) children += wsClient else throw new Manager.ClosedException(this) )
         def deregister( wsClient : WSClient ) : Unit = this.synchronized( if ( children != null ) children -= wsClient else throw new Manager.ClosedException(this) )
         def closeChildren() : Unit = this.synchronized{
           children.foreach( child => WARNING.attemptWithLabel( s"Attempt by ${this} to autoclose unclosed Exchanger.Play.WSClient failed" )( child.close() ) )
@@ -43,21 +47,34 @@ object Exchanger {
         }
       }
 
-      def apply( url : String, clientConfig : ahc.AhcWSClientConfig /* = DefaultAhcClientConfig */, manager : Manager ) : WSClient = {
+      val DefaultAhcClientConfig = {
+        val wsClientConfig = WSClientConfig( followRedirects = false, useProxyProperties = false )
+        ahc.AhcWSClientConfig( maxConnectionsPerHost = 5, maxConnectionsTotal = 5 )
+      }
+      def apply( url : String, clientConfig : ahc.AhcWSClientConfig = DefaultAhcClientConfig )( implicit manager : Manager ) : WSClient = {
         manager.synchronized {
           val configBuilder = new ahc.AhcConfigBuilder( clientConfig )
-          val out = new WSClient( url, new ahc.AhcWSClient( configBuilder.build() )( manager.materializer ), manager )
+          val out = new WSClient( url, ahc.AhcWSClient( configBuilder.build() )( manager.materializer ), Some(manager) )
           manager.register( out )
           out
         }
       }
+      // because only one overload can have default args, and we can only overload on 1st argument lost
+      def _apply( url : String, clientConfig : ahc.AhcWSClientConfig )( implicit materializer : ActorMaterializer ) : WSClient = {
+        val configBuilder = new ahc.AhcConfigBuilder( clientConfig )
+        new WSClient( url, ahc.AhcWSClient( configBuilder.build() )( materializer ), None )
+      }
+      def apply( url : String, clientConfig : ahc.AhcWSClientConfig )( implicit materializer : ActorMaterializer ) : WSClient = _apply( url, clientConfig )( materializer )
+      def apply( url : String )( implicit materializer : ActorMaterializer ) : WSClient                                       = _apply( url, DefaultAhcClientConfig )( materializer )
     }
-    class WSClient( url : String, client : play.api.libs.ws.WSClient, manager : WSClient.Manager ) extends Exchanger {
+    class WSClient private ( url : String, client : play.api.libs.ws.WSClient, mbManager : Option[WSClient.Manager] ) extends Exchanger {
       def exchange( paramsValue : JsValue )( implicit ec : ExecutionContext ) : Future[Response] = {
         client.url( url ).withFollowRedirects( false ).post( JsObject( Seq( "params" ->  paramsValue ) ) ).map( wsresponse => wsresponse.json.as[Response] )
       }
       def close() : Unit = {
-        WARNING.attemptWithLabel("Attempt to deregister an Exchanger.Play.WSClient failed")( manager.deregister( this ) )
+        mbManager.foreach{ manager =>
+          WARNING.attemptWithLabel("Attempt to deregister an Exchanger.Play.WSClient failed")( manager.deregister( this ) )
+        }
         client.close()
       }
     }
