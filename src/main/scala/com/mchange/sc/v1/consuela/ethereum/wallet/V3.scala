70f973f5fbcdf6cc6fc228287abace7d6ebd6df6
Steve Waldman <swaldman@mchange.com>
2016-06-11 02:57:10 -0700

    V3 wallets now seem to decode properly.

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/wallet/V3.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/wallet/V3.scala
@@ -2,8 +2,8 @@ package com.mchange.sc.v1.consuela.ethereum.wallet
 
 import com.mchange.sc.v1.consuela._
 import com.mchange.sc.v1.consuela.crypto.jce
-import com.mchange.sc.v1.consuela.ethereum.EthPrivateKey
-import com.mchange.sc.v1.consuela.ethereum.specification.Types.ByteSeqExact32
+import com.mchange.sc.v1.consuela.ethereum.{EthAddress,EthHash,EthPrivateKey}
+import com.mchange.sc.v1.consuela.ethereum.specification.Types.{ByteSeqExact20,ByteSeqExact32}
 
 import play.api.libs.json._
 
@@ -22,11 +22,32 @@ object V3 {
   private val MainKeyCryptAlgoName = "AES"
 
   def decodePrivateKey( jsv : JsValue, passphrase : String )( implicit provider : jce.Provider ) : EthPrivateKey = {
+
+    val _version = version( jsv )
+
+    if (_version != 3)
+      throw new WalletException( s"Excepted a V3 wallet but found version ${_version}" )
+
     val _cipher          = cipher( jsv )( provider )
-    val _key             = findKey( jsv, passphrase )( provider )
+    val _keyMac          = findKeyMac( jsv, passphrase )( provider )
     val _ivParameterSpec = new IvParameterSpec( iv(jsv) )
     val _ciphertext      = ciphertext( jsv )
-    decodePrivateKey( _cipher, _key, _ivParameterSpec, _ciphertext )
+
+    val _mac = mac( jsv )
+
+    if ( _keyMac.mac( _ciphertext ) != _mac )
+      throw new V3.WalletException( s"Message authetication code mismatch: KDF-derived MAC: ${_keyMac.mac( _ciphertext )}, expected MAC: ${_mac}" )
+
+    val out = decodePrivateKey( _cipher, _keyMac.key, _ivParameterSpec, _ciphertext )
+
+    val expectedAddress = address( jsv )
+    val pvtKeyAddress   = out.toPublicKey.toAddress
+
+    if ( pvtKeyAddress != expectedAddress ) {
+      throw new WalletException( s"Wallet is for address ${expectedAddress}, but decoded a private key for address '${pvtKeyAddress}'" )
+    } else {
+      out
+    }
   }
 
   private def decodePrivateKey( cipher : Cipher, key : SecretKey, ivParameterSpec : IvParameterSpec, ciphertext : Array[Byte] ) : EthPrivateKey = {
@@ -34,7 +55,7 @@ object V3 {
     EthPrivateKey( ByteSeqExact32( cipher.doFinal( ciphertext ) ) )
   }
 
-  private def findKey( jsv : JsValue, passphrase : String )( implicit provider : jce.Provider ) : SecretKey = {
+  private def findKeyMac( jsv : JsValue, passphrase : String )( implicit provider : jce.Provider ) : KeyMac = {
 
     val _salt = salt( jsv )
 
@@ -55,22 +76,52 @@ object V3 {
     }
   }
 
-  private def findSecretKeyPBE( algoName : String, passphrase : String, salt : Array[Byte], iterationCount : Int, derivedKeyLength : Int )( implicit provider : jce.Provider ) : SecretKey = {
-    val factory = SecretKeyFactory.getInstance( algoName, provider.name )
-    val keySpec = new PBEKeySpec( passphrase.toCharArray, salt, iterationCount, derivedKeyLength )
-    factory.generateSecret(keySpec);
+  private def findSecretKeyPBE( algoName : String, passphrase : String, salt : Array[Byte], iterationCount : Int, derivedKeyLength : Int )( implicit provider : jce.Provider ) : KeyMac = {
+    new KeyMac( findRawSecretKeyPBE( algoName, passphrase, salt, iterationCount, derivedKeyLength )( provider ) )
   }
 
-  private def findSecretKeyScrypt( passphrase : Array[Byte], salt : Array[Byte], iterationCount : Int, r : Int, p : Int, derivedKeyLength : Int )( implicit provider : jce.Provider ) : SecretKeySpec = {
+  private def findRawSecretKeyPBE( algoName : String, passphrase : String, salt : Array[Byte], iterationCount : Int, derivedKeyLength : Int )( implicit provider : jce.Provider ) : Array[Byte] = {
+    /*
+    // Grr.. bouncycastle JCE implementation doesn't yet support algo name "PBKDF2WithHmacSHA256" yet 
+
+    val factory = SecretKeyFactory.getInstance( algoName, provider.name )
+    val keySpec = new PBEKeySpec( passphrase.toCharArray, salt, iterationCount, derivedKeyLength )
+    factory.generateSecret(keySpec).getEncoded()
+    */
+
+    jce.Provider.warnForbidUnavailableProvider( this, jce.Provider.BouncyCastle )( provider ) // we can't do this via JCE, using bouncycastle directly
+
+    //import org.bouncycastle.crypto.digests.KeccakDigest
+    import org.bouncycastle.crypto.digests.SHA256Digest
+    import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator
+    import org.bouncycastle.crypto.params.KeyParameter
+
+    // modified from http://stackoverflow.com/questions/22580853/reliable-implementation-of-pbkdf2-hmac-sha256-for-java
+    val gen = new PKCS5S2ParametersGenerator(new SHA256Digest())
+    gen.init(passphrase.getBytes("UTF-8"), salt, iterationCount)
+    gen.generateDerivedParameters(derivedKeyLength * 8).asInstanceOf[KeyParameter].getKey()
+  }
+
+  private def findSecretKeyScrypt( passphrase : Array[Byte], salt : Array[Byte], iterationCount : Int, r : Int, p : Int, derivedKeyLength : Int )( implicit provider : jce.Provider ) : KeyMac = {
+    new KeyMac( findRawSecretKeyScrypt( passphrase, salt, iterationCount, r, p, derivedKeyLength )( provider ) )
+  }
+
+  private def findRawSecretKeyScrypt( passphrase : Array[Byte], salt : Array[Byte], iterationCount : Int, r : Int, p : Int, derivedKeyLength : Int )( implicit provider : jce.Provider ) : Array[Byte] = {
     jce.Provider.warnForbidUnavailableProvider( this, jce.Provider.BouncyCastle )( provider ) // we can't do this via JCE, using bouncycastle directly
 
     import org.bouncycastle.crypto.generators.SCrypt
 
-    val raw = SCrypt.generate( passphrase, salt, iterationCount, r, p, derivedKeyLength )
-    new SecretKeySpec( raw, MainKeyCryptAlgoName )
+    SCrypt.generate( passphrase, salt, iterationCount, r, p, derivedKeyLength )
   }
 
-  private def dklen( jsv : JsValue )      : Int = ( jsv \ "crypto" \ "kdfparams" \ "dklen" ).as[Int]
+  private final class KeyMac( rawKey : Array[Byte] ) {
+    val key = new SecretKeySpec( rawKey.slice(0,16), MainKeyCryptAlgoName )
+    def mac( ciphertext : Array[Byte] ) = EthHash.hash(rawKey.slice(16,32) ++ ciphertext).bytes
+  }
+
+  private def address( jsv : JsValue ) : EthAddress = EthAddress( ByteSeqExact20( ( jsv \ "address" ).as[String].decodeHex ) )
+
+  private def dklen( jsv : JsValue ) : Int = ( jsv \ "crypto" \ "kdfparams" \ "dklen" ).as[Int]
 
   private def ciphertext( jsv : JsValue ) : Array[Byte] = assertByteArray( jsv \ "crypto" \ "ciphertext" )
 
@@ -81,8 +132,12 @@ object V3 {
 
   private def kdf( jsv : JsValue ) : String = ( jsv \ "crypto" \ "kdf" ).as[String]
 
-  private def iv( jsv : JsValue )         : Array[Byte] = assertByteArray( jsv \ "crypto" \ "cipherparams" \ "iv" )
-  private def salt( jsv : JsValue )       : Array[Byte] = assertByteArray( jsv \ "crypto" \ "kdfparams" \ "salt" )
+  private def mac( jsv : JsValue ) = assertByteArray( jsv \ "crypto" \ "mac" ).toImmutableSeq // we will do an equality check, so let's not use arrays
+
+  private def iv( jsv : JsValue )   : Array[Byte] = assertByteArray( jsv \ "crypto" \ "cipherparams" \ "iv" )
+  private def salt( jsv : JsValue ) : Array[Byte] = assertByteArray( jsv \ "crypto" \ "kdfparams" \ "salt" )
+
+  private def version( jsv : JsValue ) : Int = ( jsv \ "version" ).as[Int]
 
   private def assertByteArray( jslr : JsLookupResult ) : Array[Byte] = jslr.as[String].decodeHex
 
@@ -96,7 +151,7 @@ object V3 {
     def c( jsv : JsValue ) : Int = (jsv \ "crypto" \ "kdfparams" \ "c").as[Int]
 
     def prfAsAlgoName( jsv : JsValue ) : String = {
-      jsv.as[String] match {
+      ( jsv \ "crypto" \ "kdfparams" \ "prf" ).as[String] match {
         case "hmac-sha256" => Pbkdf2AlgoName
         case other         => throw new WalletException( s"Unexpected prf value '${other}'" )
       }
---
+++ src/test/scala/com/mchange/sc/v1/consuela/ethereum/wallet/V3Spec.scala
@@ -0,0 +1,78 @@
+package com.mchange.sc.v1.consuela.ethereum.wallet;
+
+import org.specs2._;
+
+import com.mchange.sc.v1.log.MLogger;
+import com.mchange.sc.v1.log.MLevel._;
+
+import play.api.libs.json._
+
+class V3Spec extends Specification {
+
+  private implicit val logger = MLogger( this );
+
+  def is =
+s2"""
+   A V3 wallet should
+       decode to a scrypt-encoded private key consistent with its expected address                              ${e1}
+       decode to a pbkdf2-encoded private key consistent with its expected address                              ${e2}
+  """
+
+  val jsonScrypt = {
+    Json.parse (
+      """{"address":"27dd70735fa8cb93c8164b0b43bf733d71638497","crypto":{"cipher":"aes-128-ctr","ciphertext":"c4a499a3b73ebdddaf3526f4b4a996e8012465b0675e66e80f8b2900c9198247","cipherparams":{"iv":"936f111b876c5adba4e4d8b72af13e7d"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"a6fd91afc2fcbf9a61b43894120077090d6330b5e80e6bca9dcce8e5746a08f4"},"mac":"0a600717ee75a350a1d9204ec640df8541d709fe00fd3f6507644ebe69b50896"},"id":"0b1ebaf7-bfdb-4662-9a8e-6c024d024d27","version":3}"""
+    )
+  }
+
+  val passphraseScrypt = "test"
+
+  val jsonPbkdf2 = Json.parse {
+    """{
+            "address" : "008aeeda4d805471df9b2a5b0f38a0c3bcba786b",
+            "crypto" : {
+                "cipher" : "aes-128-ctr",
+                "cipherparams" : {
+                    "iv" : "6087dab2f9fdbbfaddc31a909735c1e6"
+                },
+                "ciphertext" : "5318b4d5bcd28de64ee5559e671353e16f075ecae9f99c7a79a38af5f869aa46",
+                "kdf" : "pbkdf2",
+                "kdfparams" : {
+                    "c" : 262144,
+                    "dklen" : 32,
+                    "prf" : "hmac-sha256",
+                    "salt" : "ae3cd4e7013836a3df6bd7241b12db061dbe2c6785853cce422d148a624ce0bd"
+                },
+                "mac" : "517ead924a9d0dc3124507e3393d175ce3ff7c1e96529c6c555ce9e51205e9b2"
+            },
+            "id" : "3198bc9c-6672-5ab3-d995-4942343ae5b6",
+            "version" : 3
+       }"""
+  }
+
+  val passphrasePbkdf2 = "testpassword"
+
+  def e1 : Boolean = {
+    try {
+      V3.decodePrivateKey( jsonScrypt, passphraseScrypt )
+      true
+    } catch {
+      case we : V3.WalletException => {
+        SEVERE.log("Addresses fail to match, probably, see Exception",we)
+        false
+      }
+    }
+  }
+
+  def e2 : Boolean = {
+    try {
+      V3.decodePrivateKey( jsonPbkdf2, passphrasePbkdf2 )
+      true
+    } catch {
+      case we : V3.WalletException => {
+        SEVERE.log("Addresses fail to match, probably, see Exception",we)
+        false
+      }
+    }
+  }
+
+}
