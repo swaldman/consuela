e8baccdb5a8f62afd768d8e0c5b459709f72e615
Steve Waldman <swaldman@mchange.com>
2016-08-16 17:57:22 -0700

    Implement ethabi.Encoders for fixed

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/ethabi/Encoder.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/ethabi/Encoder.scala
@@ -20,7 +20,8 @@ object Encoder {
   private val ZeroByte = 0.toByte
   private val OneByte  = 1.toByte
 
-  private val TwoBigInt = BigInt(2)
+  private val TwoBigInt     = BigInt(2)
+  private val TwoBigDecimal = BigDecimal(2)
 
   private val Mappables : Map[String, Encoder[_]] = {
     val fixedLenByteArrayBindings : Seq[Tuple2[String,Encoder[_]]] = (1 to 32).map( len => s"bytes${len}" -> new Encoder.PredefinedByteArray(len) )
@@ -42,6 +43,50 @@ object Encoder {
     Map( allBindings : _* )
   }
 
+  private lazy val UInt160 = Mappables("uint160").asInstanceOf[Encoder[BigInt]]
+  private lazy val UInt256 = Mappables("uint256").asInstanceOf[Encoder[BigInt]]
+  private lazy val SInt256 = Mappables("int256").asInstanceOf[Encoder[BigInt]]
+
+
+  private def xfixed( m : Int, n : Int, internal : Encoder[BigInt], signed : Boolean ) : Encoder[BigDecimal] = {
+    new Encoder[BigDecimal] {
+      require( m + n <= 256 )
+
+      val denominator = TwoBigDecimal.pow(n)
+
+      def notRepresentableMessage( bd : BigDecimal ) = s"${bd} cannot be represented as a multiple of 1/(2 ** ${n})"
+      def downToBigDecimal( bi : BigInt ) = Failable( BigDecimal( bi ) / TwoBigDecimal.pow(n) )
+      def upToBigInt( bd : BigDecimal )   = Failable( bd * denominator ).withFilter( _.isWhole ).map( _.toBigInt ).xmap( _ => Fail.simple( notRepresentableMessage( bd ) ) )
+      def isRepresentable( bd : BigDecimal ) = (bd * denominator).isWhole
+      def representableOrFail( bd : BigDecimal ) = isRepresentable( bd ).toFailable( notRepresentableMessage( bd ) ).map( _ => bd )
+
+      def parse( str : String ) : Failable[BigDecimal] = {
+        for {
+          raw <- Failable( BigDecimal( str ) )
+          _ <- ( signed || raw >= 0 ).toFailable("Unsigned parser can't parse values below zero.")
+          result <- representableOrFail( raw )
+        } yield {
+          result
+        }
+      }
+      def format( representation : BigDecimal ) : Failable[String] = {
+       representableOrFail( representation ).map( _.toString )
+      }
+      def encode( representation : BigDecimal ) : Failable[immutable.Seq[Byte]] = {
+        upToBigInt( representation ).flatMap( internal.encode )
+      }
+      def decode( bytes : immutable.Seq[Byte] ) : ( Failable[BigDecimal], immutable.Seq[Byte] ) = {
+        val ( bi, rest ) = internal.decode( bytes )
+        ( bi.flatMap( downToBigDecimal ), rest )
+      }
+      def decodeComplete( bytes : Seq[Byte] ) : Failable[BigDecimal] = {
+        internal.decodeComplete( bytes ).flatMap( downToBigDecimal )
+      }
+    }
+  }
+
+  private def  fixed( m : Int, n : Int ) = xfixed( m, n, SInt256, true )
+  private def ufixed( m : Int, n : Int ) = xfixed( m, n, UInt256, false )
 
   final object Bool extends FixedLengthRepresentation[Boolean]( 32 ) {
     def parse( str : String ) : Failable[Boolean] = {
@@ -63,20 +108,18 @@ object Encoder {
     }
   }
   final object Address extends Encoder[EthAddress] {
-    private val internal = new UInt(160) // we could reuse the UInt(160) we generate above but this seems complicated
-
     private def toAddress( bi : BigInt )         : EthAddress = EthAddress( ByteSeqExact20( asFixedLengthUnsignedByteArray( bi, 20 ).toImmutableSeq ) )
     private def toBigInt( address : EthAddress ) : BigInt     = BigInt( 1, address.bytes.widen.toArray )
 
-    def parse( str : String ) : Failable[EthAddress]             = internal.parse( str ).map( toAddress )
-    def format( representation : EthAddress ) : Failable[String] = internal.format( toBigInt( representation ) )
+    def parse( str : String ) : Failable[EthAddress]             = Failable( EthAddress( str ) )
+    def format( representation : EthAddress ) : Failable[String] = Failable( "0x"+representation.hex )
 
-    def encode( representation : EthAddress ) : Failable[immutable.Seq[Byte]]                 = internal.encode( toBigInt( representation ) )
+    def encode( representation : EthAddress ) : Failable[immutable.Seq[Byte]]                 = UInt160.encode( toBigInt( representation ) )
     def decode( bytes : immutable.Seq[Byte] ) : ( Failable[EthAddress], immutable.Seq[Byte] ) = {
-      val tup = internal.decode( bytes )
+      val tup = UInt160.decode( bytes )
       ( tup._1.map( toAddress ), tup._2 )
     }
-    def decodeComplete( bytes : Seq[Byte] ) : Failable[EthAddress] = internal.decodeComplete( bytes ).map( toAddress )
+    def decodeComplete( bytes : Seq[Byte] ) : Failable[EthAddress] = UInt160.decodeComplete( bytes ).map( toAddress )
   }
   final class UInt( bitLen : Int ) extends FixedLengthRepresentation[BigInt]( 32 ) {
     require( bitLen % 8 == 0 )
