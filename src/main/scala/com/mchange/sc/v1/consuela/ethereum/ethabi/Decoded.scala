6f01051cf0aa506f067f713d223992c753aabfd5
Steve Waldman <swaldman@mchange.com>
2019-03-18 17:27:20 -0700

    More accurate comment to explain the purpose of Decoded.Hash

--- src/main/scala/com/mchange/sc/v1/consuela/ethereum/ethabi/Decoded.scala
+++ src/main/scala/com/mchange/sc/v1/consuela/ethereum/ethabi/Decoded.scala
@@ -5,7 +5,7 @@ import jsonrpc.Abi
 
 object Decoded {
   case class Value( parameter : Abi.Parameter, value : Any, stringRep : String ) extends Decoded
-  case class Hash( parameter : Abi.Parameter, hash : EthHash ) extends Decoded // event arguments of dynamic types can only be decoded has hashes :(
+  case class Hash( parameter : Abi.Parameter, hash : EthHash ) extends Decoded // indexed event arguments of dynamic types can only be decoded as hashes :(
 }
 sealed trait Decoded {
   def parameter : Abi.Parameter
