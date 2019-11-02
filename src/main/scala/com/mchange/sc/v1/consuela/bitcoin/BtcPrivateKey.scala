ef2cac44d19aefd93ecbd630379662e55fc68ad4
Steve Waldman <swaldman@mchange.com>
2019-11-01 18:54:04 -0700

    First play at BtcPrivateKey.

---
+++ src/main/scala/com/mchange/sc/v1/consuela/bitcoin/BtcPrivateKey.scala
@@ -0,0 +1,54 @@
+package com.mchange.sc.v1.consuela.bitcoin
+
+import encoding.Base58
+
+import com.mchange.sc.v1.consuela._
+import com.mchange.sc.v1.consuela.ethereum.EthPrivateKey
+
+object BtcPrivateKey {
+  def fromHex( privateKeyHex : String ) : BtcPrivateKey = new BtcPrivateKey( new EthPrivateKey( ByteSeqExact32( privateKeyHex.decodeHex ) ) )
+
+  def fromBase64( base64String : String ) : BtcPrivateKey = new BtcPrivateKey( new EthPrivateKey( ByteSeqExact32( Base64Decoder.decode( base64String ) ) ) )
+
+  val CompressedMarker = 0x01.toByte
+
+  final object Header {
+    val Mainnet = 0x80.toByte
+    val Testnet = 0xef.toByte
+  }
+
+  // javadoc: "Instances of Base64.Encoder class are safe for use by multiple concurrent threads."
+  private lazy val Base64Encoder = java.util.Base64.getEncoder()
+
+  // javadoc: "Instances of Base64.Decoder class are safe for use by multiple concurrent threads."
+  private lazy val Base64Decoder = java.util.Base64.getDecoder()
+
+  private final val ArbitraryHashCodeOffset = 17698
+
+}
+final class BtcPrivateKey private ( val toEthPrivateKey : EthPrivateKey )  {
+  import BtcPrivateKey._
+
+  private [this] lazy val raw = toEthPrivateKey.bytes.widen.toArray
+
+  def hex = toEthPrivateKey.hex
+
+  lazy val base64 = Base64Encoder.encodeToString( raw )
+
+  def wif( mainnet : Boolean = true, compressed : Boolean = true ) : String = {
+    val header  = if ( mainnet ) Header.Mainnet else Header.Testnet
+    val payload = if ( compressed ) raw :+ CompressedMarker else raw
+    Base58.encodeChecked( header, payload )
+  }
+
+  override def equals( other : Any ) : Boolean = {
+    other match {
+      case bpk : BtcPrivateKey => this.toEthPrivateKey == bpk.toEthPrivateKey
+      case _                   => false
+    }
+  }
+
+  override def hashCode() : Int = toEthPrivateKey.hashCode + ArbitraryHashCodeOffset
+
+  override def toString() : String = "BtcPrivateKey(ByteSeqExact32(<masked>))"
+}
---
+++ src/main/scala/com/mchange/sc/v1/consuela/bitcoin/package.scala
@@ -0,0 +1,10 @@
+package com.mchange.sc.v1.consuela
+
+package object bitcoin {
+  val  ByteSeqExact32 = com.mchange.sc.v1.consuela.ethereum.specification.Types.ByteSeqExact32
+  type ByteSeqExact32 = com.mchange.sc.v1.consuela.ethereum.specification.Types.ByteSeqExact32
+  
+  val  ByteSeqExact65 = com.mchange.sc.v1.consuela.ethereum.specification.Types.ByteSeqExact65
+  type ByteSeqExact65 = com.mchange.sc.v1.consuela.ethereum.specification.Types.ByteSeqExact65
+  
+}
