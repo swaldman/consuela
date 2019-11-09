f538c99bcfe401da2443a8c4d882241f9e368b6f
Steve Waldman <swaldman@mchange.com>
2019-11-08 22:18:37 -0800

    Define some SLIP-44 constants.

---
+++ src/main/scala/com/mchange/sc/v1/consuela/slip/Slip44.scala
@@ -0,0 +1,10 @@
+package com.mchange.sc.v1.consuela.slip
+
+// see https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+
+object Slip44 {
+  final object Index {
+    val Bitcoin  = 0
+    val Ethereum = 60
+  }
+}
